9f43f6d5ee9a09db795acc5d33c915ec2f284c90
g <gabe.patgorski@gmail.com>
2024-02-02 09:52:37 -0800

    404

---
+++ src/pages/404.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main
+      className="login-background"
+    >
+      <div className="text-white p-2">your picks are locked in. admin will pay out after super bowl</div>
+    
+    </main>
+  );
+}
