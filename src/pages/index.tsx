4d8d454e9eb9462b2f1a53840449fe09463c31f0
g <gabe.patgorski@gmail.com>
2024-02-02 09:41:25 -0800

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ export default function Home() {
     >
       <div className="text-white p-2">your picks are locked in. admin will pay out after super bowl</div>
     
+    <div>the end</div>
     </main>
   );
 }
